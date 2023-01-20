49fe38c99476ba0515c3e054fc88cf01b11bbbf5
RedginaldGodeau <5657@holbertonstudents.com>
2023-01-20 13:25:32 -0800

    adding sentence

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,7 +3,12 @@ import sys
 
 if __name__ == '__main__':
     args = sys.argv[1:]
+    sentence = "argument."
+    if (len(args) > 1):
+        sentence = "arguments:"
+    elif (len(args) == 1):
+        sentence = "argument:"
 
-    print("{:} arguments:".format(len(args)))
+    print("{:} {:}".format(len(args), sentence))
     for arg in range(len(args)):
         print("{:}: {:}".format(arg + 1, args[arg])) 
