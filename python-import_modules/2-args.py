92c155c4642b583ca880e745d8ff2f44a5533549
RedginaldGodeau <5657@holbertonstudents.com>
2023-01-20 13:22:36 -0800

    adding format

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,6 +4,6 @@ import sys
 if __name__ == '__main__':
     args = sys.argv[1:]
 
-    print("{:} arguments:")
+    print("{:} arguments:".format(len(args)))
     for arg in range(len(args)):
         print("{:}: {:}".format(arg + 1, args[arg])) 
