db0152fc22bcde76cd983525cc5d28ed1b18a9fb
RedginaldGodeau <5657@holbertonstudents.com>
2023-01-20 13:17:55 -0800

    finish project

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,4 +4,4 @@ import sys
 if __name__ == '__main__':
     args = sys.argv[1:]
     for arg in range(len(args)):
-        print("{:}: {:}".format(arg, args[arg])) 
+        print("{:}: {:}".format(arg + 1, args[arg])) 
