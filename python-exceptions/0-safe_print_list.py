2008e6626f9503e459a4ecac380f66c77b0c2798
RedginaldGodeau <5657@holbertonstudents.com>
2023-01-27 13:59:55 -0800

    adding p3

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for item in my_list:
+            print(f"{item}", end = " ")
+        print()
+        return (my_list[x])
+    except:
+        return (my_list[-1])
---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    
+    for i in range(list_length):
+        try:
+            new_list.append(my_list_1[i] / my_list_2[i])
+        except TypeError as err:
+            new_list.append(0)
+            print("wrong type")
+        except ZeroDivisionError as err:
+            new_list.append(0)
+            print("division by 0")
+        except IndexError as err:
+            new_list.append(0)
+            print("out of range")
+    return (new_list)
