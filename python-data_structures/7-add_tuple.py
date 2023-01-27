5c7e03f1da0a4ff602e53cef5cccf2bd08e4dc2c
RedginaldGodeau <5657@holbertonstudents.com>
2023-01-27 12:32:52 -0800

    coor 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -8,7 +8,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     newtup = (tup1[0] + (0 if len(tup2) <= 0 else tup2[0]),)
 
     for t in range(1, len(tup1)):
-        b = 0 if len(tup2) <= t else tup2[t]
-        newtup += ((0 if len(tup2) <= t else tup2[t]) + tup1[t],)
-
+        newtup += (tup2, tup1)
     return (newtup)
