d59aa6a7c95a1cb9684a6c4ea6fb11dc101e5c6e
RedginaldGodeau <5657@holbertonstudents.com>
2023-01-27 14:45:09 -0800

    retry

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
+        if not my_list:
+            print()
+            return (0)
+
         for item in my_list:
-            print(f"{item}", end = "")
+            print(f"{item}", end="")
         print()
         return (my_list[x])
     except:
