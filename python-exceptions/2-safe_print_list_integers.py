025262ff3ea017ad4fbb0c9fe7895f2c3898eadc
RedginaldGodeau <5657@holbertonstudents.com>
2023-01-27 14:25:00 -0800

    corr

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     try:
+        if len(my_list) == 0:
+            return (0)
+
         for i in range(x):
             print("{:d}".format(my_list[i]), end="")
         print()
