dbbaed8a7ed0f0fd54bf873a2ff307e83b5811ad
RedginaldGodeau <5657@holbertonstudents.com>
2023-01-27 14:43:05 -0800

    corr

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for item in my_list:
-            print(f"{item}", end = " ")
+            print(f"{item}", end = "")
         print()
         return (my_list[x])
     except:
