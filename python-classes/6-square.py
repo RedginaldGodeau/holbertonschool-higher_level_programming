5d1252bd54b2b4ccb7eb836094b5b9260ce74cd1
RedginaldGodeau <5657@holbertonstudents.com>
2023-02-01 10:57:41 -0800

    corr

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -16,7 +16,9 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-        if (not all(isinstance(v, int) for v in position) or position[1] * position[0] < 0):
+        if (not all(isinstance(v, int) for v in position) 
+        or position[1] * position[0] < 0
+        or len(position) != 2):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = position
@@ -41,7 +43,9 @@ class Square:
         return (self.__position)
 
     def setposition(self, value):
-        if (not not all(isinstance(v, int) for v in position) or value[1] * value[0] < 0):
+        if (not all(isinstance(v, int) for v in position) 
+        or value[1] * value[0] < 0
+        or len(position) != 2):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = position
