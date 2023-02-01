d5cac878f3c3788f0a8c7e646a0977f0f1a6165c
RedginaldGodeau <5657@holbertonstudents.com>
2023-02-01 10:21:18 -0800

    add t5

---
+++ python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""A Python Script"""
+
+
+class Square:
+    """
+    Make a Square
+    Args:
+        __size (int): size.
+    """
+
+    def __init__(self, size=0):
+        if (not isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def getsize(self):
+        return (self.__size)
+
+    def setsize(self, value):
+        if (not isinstance(value, int)):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+        return (self.__size)
+
+    size = property(getsize, setsize)
+
+    def my_print(self):
+        for i in range(self.__size):
+            for i2 in range(self.__size):
+                print("#", end="")
+            print()
