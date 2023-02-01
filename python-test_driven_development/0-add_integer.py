d0f71cbfbf50cc437aa0be9b8124912a87007885
RedginaldGodeau <5657@holbertonstudents.com>
2023-02-01 11:34:53 -0800

    adding readme

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,16 +10,3 @@ def add_integer(a, b=98):
         f = open("tests/0-add_integer.txt", "a")
         f.write(str(err))
         f.close()
-
-print(add_integer(1, 2))
-print(add_integer(100, -2))
-print(add_integer(2))
-print(add_integer(100.3, -2))
-try:
-    print(add_integer(4, "School"))
-except Exception as e:
-    print(e)
-try:
-    print(add_integer(None))
-except Exception as e:
-    print(e)
