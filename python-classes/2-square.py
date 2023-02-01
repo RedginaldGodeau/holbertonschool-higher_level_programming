adcf5655318d765c21b996f14ea04a8019de2358
RedginaldGodeau <5657@holbertonstudents.com>
2023-02-01 07:41:36 -0800

    corr

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -13,7 +13,7 @@ class Square:
         try:
             if (size < 0):
                 raise ValueError
-            elif (isinstance(<var>, int)
+            elif (isinstance(size, int)):
                 raise TypeError;
             self.__size = size
         except ValueInf:
