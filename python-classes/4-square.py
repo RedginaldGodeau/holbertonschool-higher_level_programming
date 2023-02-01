649c661fa983939d1048993fac333bbfde1a473f
RedginaldGodeau <5657@holbertonstudents.com>
2023-02-01 10:11:38 -0800

    corr

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -17,17 +17,20 @@ class Square:
         else:
             self.__size = size
 
+
+
     def area(self):
         return (self.__size * self.__size)
 
-    def size(self):
+    def getsize(self):
         return (self.__size)
     
-    def size(self, value):
+    def setsize(self, value):
         if (not isinstance(value, int)):
             raise TypeError("size must be an integer")
         elif (value < 0):
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
         return (self.__size)
+
+    size = property(getsize, setsize)
