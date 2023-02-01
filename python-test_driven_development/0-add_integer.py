64749ae3ec5c5e069a3960096fc35aa6fe6684d0
RedginaldGodeau <5657@holbertonstudents.com>
2023-02-01 11:42:33 -0800

    corr

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+""" Doc """
+
 def add_integer(a, b=98):
+     """ Doc """
     if (not isinstance(a, (int, float))):
         raise TypeError("a must be an integer")
     elif (not isinstance(b, (int, float))):
