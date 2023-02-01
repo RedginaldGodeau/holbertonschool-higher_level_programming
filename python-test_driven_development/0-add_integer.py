72e94517ef581a88df7a7c3718010b02cc55345c
RedginaldGodeau <5657@holbertonstudents.com>
2023-02-01 11:40:47 -0800

    corr

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    try:
-        if (not isinstance(a, int)):
-            raise TypeError("a must be an integer")
-        elif (not isinstance(b, int)):
-            raise TypeError("b must be an integer")
-        return (a + b)
-    except TypeError as err:
-        f = open("tests/0-add_integer.txt", "a")
-        f.write(str(err))
-        f.close()
+    if (not isinstance(a, (int, float))):
+        raise TypeError("a must be an integer")
+    elif (not isinstance(b, (int, float))):
+        raise TypeError("b must be an integer")
+    else:
+        return (int) (a + b)
+
