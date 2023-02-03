a8e5ef7a8667c5f4481930d8f76a20b09a4aea2e
RedginaldGodeau <5657@holbertonstudents.com>
2023-02-03 12:25:32 -0800

    add t5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Doc """
+
+def text_indentation(text):
+    """ Doc """
+
+    if text == None or not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    spaceCheck = False
+    for letter in text:
+        if "?.:".find(letter) != -1:
+            print(letter, end="")
+            print()
+            spaceCheck = True
+        else:
+            if spaceCheck and letter == ' ':
+                pass
+            else:
+                print(letter, end="")
+                spaceCheck = False
