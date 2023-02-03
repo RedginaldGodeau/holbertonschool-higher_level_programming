e997c1c131a81a91c56cda5d0b80aef3777d0050
RedginaldGodeau <5657@holbertonstudents.com>
2023-02-03 14:09:49 -0800

    corr

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -60,5 +60,5 @@ class Rectangle:
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if rect_1.area() >= rect_2.area():
-            return (True)
-        return (False)
+            return (rect_1)
+        return (rect_2)
