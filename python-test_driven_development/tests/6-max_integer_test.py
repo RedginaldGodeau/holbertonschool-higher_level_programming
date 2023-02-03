4bf65b47351f58457e1d2d6c15a389843ad05085
RedginaldGodeau <5657@holbertonstudents.com>
2023-02-03 12:51:38 -0800

    test

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -33,7 +33,7 @@ class FonctionTest(unittest.TestCase):
         print(max_integer([255, 2.532, 0, -5]))
     def test8(self):
         print(max_integer([0, 2.532, 255, -5, 90]))
-    def test8(self):
+    def test9(self):
         print(max_integer([0, 2.532, 255, -5, 300]))
     
 
