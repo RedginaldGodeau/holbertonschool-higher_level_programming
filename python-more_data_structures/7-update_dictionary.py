34f601fca4174fe3958749730b0b0fbf0c4f50c7
RedginaldGodeau <5657@holbertonstudents.com>
2023-02-13 05:47:00 -0800

    pep8 corr

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
-
 def update_dictionary(a_dictionary, key, value):
-
     new_key = {key: value}
 
     if key in a_dictionary:
         a_dictionary.update(new_key)
     else:
-        a_dictionary[key] = value;
+        a_dictionary[key] = value
     return a_dictionary
