ec08bdc681fb7c835105c4a076441ffe260f31e5
RedginaldGodeau <5657@holbertonstudents.com>
2023-02-13 01:19:17 -0800

    rework

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-
-    if not search in my_list:
-        return (my_list)
-
-    i = my_list.index(search)
-
     new = my_list.copy()
-    new[i] = replace
+    
+    while search in my_list:
+        i = my_list.index(search)
+        new[i] = replace
 
     return (new)
