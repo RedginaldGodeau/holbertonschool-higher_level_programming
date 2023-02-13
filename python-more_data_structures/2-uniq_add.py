48bdfdd6c01a354860a5394f7faebaee960630aa
RedginaldGodeau <5657@holbertonstudents.com>
2023-02-13 01:34:15 -0800

    Correcting files

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    return add(my_list)
+    new_list = list(dict.fromkeys(my_list))
+    n = 0
+
+    for item in new_list:
+        n += item
+
+    return (n)
