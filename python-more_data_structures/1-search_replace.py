d62fd70d2ff74ce92f26dbf558d28503d2866df1
RedginaldGodeau <5657@holbertonstudents.com>
2023-02-13 01:11:34 -0800

    correcting file

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new = my_list.copy()
-    i = new.index(search)
+    i = my_list.find(search)
 
     if i == -1:
-        return (new)
-    
+        return (my_list)
+
+    new = my_list.copy()
     new[i] = replace
+
     return (new)
