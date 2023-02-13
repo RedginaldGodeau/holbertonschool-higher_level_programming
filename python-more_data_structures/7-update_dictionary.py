2d1210cb8d0bcfbd347b728b39331f44be405f6c
RedginaldGodeau <5657@holbertonstudents.com>
2023-02-13 05:44:56 -0800

    adding corr

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
+
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary.update({key: value})
+
+    new_key = {key: value}
+
+    if key in a_dictionary:
+        a_dictionary.update(new_key)
+    else:
+        a_dictionary[key] = value;
+    return a_dictionary
