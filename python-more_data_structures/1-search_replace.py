26e4d878d87383c76c030077c30bb4df587568e1
RedginaldGodeau <5657@holbertonstudents.com>
2023-02-13 01:25:13 -0800

    correcting while loop

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 def search_replace(my_list, search, replace):
     new = my_list.copy()
     
-    while search in my_list:
+    while search in new:
         i = my_list.index(search)
         new[i] = replace
 
