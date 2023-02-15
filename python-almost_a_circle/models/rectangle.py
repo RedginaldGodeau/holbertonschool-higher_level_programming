3a8a283b9caa4f69342cd46b2663146718e02f35
RedginaldGodeau <5657@holbertonstudents.com>
2023-02-15 16:26:50 +0100

    correcting docstring

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,10 +4,22 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """docs"""
+    """Rectangle
+
+    Args:
+        Base (self): Parent class
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """docs"""
+        """__init__
+
+        Args:
+            width (int / float)
+            height (int / float)
+            x (int, optional): Defaults to 0.
+            y (int, optional): Defaults to 0.
+            id (int, optional): Defaults to None.
+        """
         Base.__init__(self, id)
         self.setheight(height)
         self.setwidth(width)
@@ -15,16 +27,32 @@ class Rectangle(Base):
         self.sety(y)
 
     def __str__(self):
-        """docs"""
+        """__str__
+
+        Returns:
+            string: Info of class
+        """
         return f"[Rectangle] ({self.id})\
  {self.x}/{self.y} - {self.width}/{self.height}"
 
     def getwidth(self):
-        """docs"""
+        """getwidth
+
+        Returns:
+            int: Width of Class
+        """
         return self.__width
 
     def setwidth(self, value):
-        """docs"""
+        """setwidth
+
+        Args:
+            value (int): value width
+
+        Raises:
+            TypeError: width must be an integer
+            ValueError: width must be > 0
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -35,11 +63,23 @@ class Rectangle(Base):
     width = property(getwidth, setwidth)
 
     def getheight(self):
-        """docs"""
+        """getheight
+
+        Returns:
+            int: get height
+        """
         return self.__height
 
     def setheight(self, value):
-        """docs"""
+        """setheight
+
+        Args:
+            value (int): value height
+
+        Raises:
+            TypeError: height must be an integer
+            ValueError: height must be > 0
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
@@ -49,11 +89,23 @@ class Rectangle(Base):
     height = property(getheight, setheight)
 
     def getx(self):
-        """docs"""
+        """getx
+
+        Returns:
+            int: X
+        """
         return self.__x
 
     def setx(self, value):
-        """docs"""
+        """setx
+
+        Args:
+            value (int): value x
+
+        Raises:
+            TypeError: x must be an integer
+            ValueError: x must be >= 0
+        """
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         elif value < 0:
@@ -64,11 +116,23 @@ class Rectangle(Base):
     x = property(getx, setx)
 
     def gety(self):
-        """docs"""
+        """gety
+
+        Returns:
+            int: get y
+        """
         return self.__y
 
     def sety(self, value):
-        """docs"""
+        """sety
+
+        Args:
+            value (int): value y
+
+        Raises:
+            TypeError: y must be an integer
+            ValueError: y must be >= 0
+        """
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         elif value < 0:
@@ -78,11 +142,19 @@ class Rectangle(Base):
     y = property(gety, sety)
 
     def area(self):
-        """docs"""
+        """area
+
+        Returns:
+            int: get area of class
+        """
         return self.x * self.y
 
     def getdisplay(self):
-        """docs"""
+        """getdisplay
+
+        Returns:
+            _type_: Rectangle printed
+        """
         rec = ""
         # for space in range(self.y):
         #    rec += "\n"
@@ -95,5 +167,7 @@ class Rectangle(Base):
         return rec
 
     def display(self):
-        """docs"""
+        """display
+            print rectangle
+        """
         print(self.getdisplay())
