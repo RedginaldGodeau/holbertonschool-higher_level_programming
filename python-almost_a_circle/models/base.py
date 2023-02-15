a1c203dd6abc00c526c6b7f7bf8bd8885ee5b66f
RedginaldGodeau <5657@holbertonstudents.com>
2023-02-15 16:05:31 +0100

    Correcting my docs

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -6,6 +6,7 @@ class Base:
     __nb_objects = 0;
     
     def __init__(self, id=None):
+        """_summary_"""
         if id != None:
             self.id = id
         else:
--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-from models.base import Base
-
 """ docs """
+from models.base import Base
 
 
 class Rectangle(Base):
