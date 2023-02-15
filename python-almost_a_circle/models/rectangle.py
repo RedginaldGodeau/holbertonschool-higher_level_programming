2c5be47f5cd2e25c1e4d6c7833bcdbc46a7ca97b
RedginaldGodeau <5657@holbertonstudents.com>
2023-02-15 15:56:32 +0100

    Correcting my docs

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""_summary_"""
-
-
 from base import Base
 
+""" docs """
+
+
 class Rectangle(Base):
-    """_summary_"""
+    """ docs"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """_summary_"""
         Base.__init__(self, id)
         self.setheight(height)
         self.setwidth(width)
@@ -22,7 +21,6 @@ class Rectangle(Base):
         return (self.__width)
 
     def setwidth(self, value):
-        """_summary_"""
         if (not isinstance(value, int)):
             raise TypeError("width must be an integer")
         elif (value < 0):
@@ -35,7 +33,6 @@ class Rectangle(Base):
         return (self.__height)
 
     def setheight(self, value):
-        """_summary_"""
         if (not isinstance(value, int)):
             raise TypeError("height must be an integer")
         elif (value < 0):
@@ -48,7 +45,6 @@ class Rectangle(Base):
         return (self.__x)
 
     def setx(self, value):
-        """_summary_"""
         if (not isinstance(value, int)):
             raise TypeError("x must be an integer")
         elif (value < 0):
@@ -61,7 +57,6 @@ class Rectangle(Base):
         return (self.__y)
 
     def sety(self, value):
-        """_summary_"""
         if (not isinstance(value, int)):
             raise TypeError("y must be an integer")
         elif (value < 0):
@@ -74,8 +69,6 @@ class Rectangle(Base):
         return self.x * self.y
 
     def getdisplay (self):
-        """doc"""
-
         rec = ""
         #for space in range(self.y):
         #    rec += "\n"
