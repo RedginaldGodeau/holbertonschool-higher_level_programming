09410923e5cdd490ffeeaa5f1b038653d55f30a9
RedginaldGodeau <5657@holbertonstudents.com>
2023-03-15 22:20:41 +0100

    adding commentary

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
+-- VIEW GRANTS
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,1 +1,2 @@
+-- CREATE USER
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
