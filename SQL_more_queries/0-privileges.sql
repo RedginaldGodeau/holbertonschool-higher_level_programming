27dbed6bd6f764412cd685b89d485448f8003160
RedginaldGodeau <5657@holbertonstudents.com>
2023-03-15 22:30:52 +0100

    trying this way

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- VIEW GRANTS
-GRANTS FOR 'user_0d_1'@'localhost';
-GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
+SHOW GRANT ALL PRIVILEGES ON *.* TO user_0d_2@localhost;
