4512beac643ba4b315fef55c0d3fb116abc027f5
RedginaldGodeau <5657@holbertonstudents.com>
2023-04-10 10:33:45 -0700

    corr 7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 const argv = process.argv.splice(2);
 
+
+
 if (parseInt(argv[0])) {
-  for (let i = 0; i > parseInt(argv[0]); i++) { console.log('C is fun'); }
+  for (let i = 0; i < parseInt(argv[0]); i++) { console.log('C is fun'); }
 } else { console.log('Missing number of occurrences'); }
