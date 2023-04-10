9e1fcd6b227d2bf9b8cd1e6587b904e5fb368ea1
RedginaldGodeau <5657@holbertonstudents.com>
2023-04-10 10:57:05 -0700

    fix style

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const argv = process.argv.splice(2);
 
 if (argv.length) {
-  console.log(argv.length > 1 ? "Arguments found" : "Argument found");
+  console.log(argv.length > 1 ? 'Arguments found' : 'Argument found');
 } else {
-  console.log("No argument");
+  console.log('No argument');
 }
--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,4 +2,4 @@
 const argv = process.argv.splice(2);
 const add = (a, b) => console.log(a + b);
 
-add(parseInt(argv[0]), parseInt(argv[1])) 
+add(parseInt(argv[0]), parseInt(argv[1]));
