44d0e67143eefe39dff5d3f1e730f5afae3ed193
RedginaldGodeau <5657@holbertonstudents.com>
2023-04-10 10:34:45 -0700

    corr 8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,5 +4,5 @@ const argv = process.argv.splice(2);
 const square = parseInt(argv[0]);
 
 if (square) {
-  for (let i = 0; i > square; i++) { console.log('x'.repeat(square)); }
+  for (let i = 0; i < square; i++) { console.log('x'.repeat(square)); }
 } else { console.log('Missing size'); }
