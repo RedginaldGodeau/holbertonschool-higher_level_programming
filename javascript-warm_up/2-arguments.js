378a19e1e8322df7b1ad098697bb15ae9a498a07
RedginaldGodeau <5657@holbertonstudents.com>
2023-04-10 19:25:28 +0200

    adding args

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
 const argv = process.argv.splice(2);
 
-if (argv.length) { console.log('Argument found'); } else { console.log('No argument'); }
+if (argv.length) {
+  console.log(argv.length > 1 ? "Arguments found" : "Argument found");
+} else {
+  console.log("No argument");
+}
