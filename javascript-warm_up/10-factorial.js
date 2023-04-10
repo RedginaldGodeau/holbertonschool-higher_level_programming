db06b8781783f893cb7406227de2f5b27774c386
RedginaldGodeau <5657@holbertonstudents.com>
2023-04-10 10:42:30 -0700

    corr 10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const argv = process.argv;
+const argv = process.argv.splice(2);
 
-const fact = (max = 1, n = 1) => {
+function fact (max = 1, n = 1) {
   if (n >= max) { return n; }
 
   return fact(max, n * (n + 1));
