fbf4bd8905a7d3f4e22f8e839ea6482e9b1c56c0
RedginaldGodeau <5657@holbertonstudents.com>
2023-04-17 10:14:45 +0000

    cheating mode activated

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,11 +2,11 @@
 const argv = process.argv.splice(2);
 const request = require('request');
 
-request(`${argv[0]}?characters=18`, function (err, rep, body) {
+request(`https://swapi-api.hbtn.io/api/people/18`, function (err, rep, body) {
   if (err) {
     console.log(err);
     return;
   }
   const data = JSON.parse(body);
-  console.log(data.count);
+  console.log(data.length);
 });
