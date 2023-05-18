4a47d8398475f210f306eb94c94a41b8824bbb62
Redginald Godeau <113888771+RedginaldGodeau@users.noreply.github.com>
2023-05-18 17:38:08 +0200

    Update 8-esrever.js

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
-
-exports.esrever = function (list) {
-  return list.map((item, idx) => array[list.length - 1 - idx]);
-};
+function esrever (list) {
+  const newList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    newList.push(list[i]);
+  }
+  return newList;
+}
+module.exports = { esrever };
