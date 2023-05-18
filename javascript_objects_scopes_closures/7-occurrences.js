747e82cd396d5562666113544ef37773376db8f9
Redginald Godeau <113888771+RedginaldGodeau@users.noreply.github.com>
2023-05-18 17:38:41 +0200

    Update 7-occurrences.js

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,21 @@
 #!/usr/bin/node
+const S1 = require('./5-square');
 
-exports.nbOccurences = function (list, searchElement) {
-  return list.slice(searchElement).length - 1;
-};
+class Square extends S1 {
+  charPrint (c) {
+    let char = '';
+    if (c === undefined) {
+      char = 'X';
+    } else {
+      char = c;
+    }
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write(char);
+      }
+      console.log();
+    }
+  }
+}
+
+module.exports = Square;
