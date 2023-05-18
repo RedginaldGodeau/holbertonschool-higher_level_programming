7ca6f676d56ff93eafefd6f7440a8dd39964ff07
Redginald Godeau <113888771+RedginaldGodeau@users.noreply.github.com>
2023-05-18 17:40:48 +0200

    Update 6-square.js

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,9 +1,21 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const S1 = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
+class Square extends S1 {
+  charPrint (c) {
+    let char = '';
+    if (c === undefined) {
+      char = 'X';
+    } else {
+      char = c;
+    }
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write(char);
+      }
+      console.log();
+    }
   }
 }
+
 module.exports = Square;
